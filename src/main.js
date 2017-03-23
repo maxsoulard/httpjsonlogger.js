a350b6e5da43e2516c2f288eb05525633f6c89bd
Maxime Soulard <max.soulard@gmail.com>
2017-03-23 23:27:02 +0100

    Route filter now in conf.json

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const handler = require('./handler');
 const app = express();
+const conf = require('../conf');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,9 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('*', handler.get);
-app.post('*', handler.post);
-app.put('*', handler.put);
-app.delete('*', handler.delete);
+app.get(conf.routeFilter, handler.get);
+/*app.post(conf.routeFilter, handler.post);
+app.put(conf.routeFilter, handler.put);
+app.delete(conf.routeFilter, handler.delete);*/
 
 app.listen('8888');
