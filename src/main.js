da9b2252aa48b01431cffe3dcec75eb898980ab7
Maxime Soulard <maxime.soulard-prestataire@labanquepostale.fr>
2017-03-24 15:25:44 +0100

    Logging DELETE requests

--- src/main.js
+++ src/main.js
@@ -13,6 +13,6 @@ app.use(bodyParser.json());
 app.get(conf.routeFilter, handler.get);
 app.post(conf.routeFilter, handler.post);
 app.put(conf.routeFilter, handler.put);
-/*app.delete(conf.routeFilter, handler.delete);*/
+app.delete(conf.routeFilter, handler.delete);
 
 app.listen('8888');
